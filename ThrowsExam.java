6229a486ed7977d81ba4906c4dc9d27f9b7f5256
Anupama_Ravala <anupamaravala@gmail.com>
2025-12-18 18:59:41 +0530

    Add files via upload

---
+++ ThrowsExam.java
@@ -0,0 +1,19 @@
+class Demo{
+    void div() throws ArithmeticException{
+        int n1=25;
+        int n2=0;
+        System.out.println(n1/n2);
+    }
+}
+public class ThrowsExam{
+    public static void main(String[] args){
+        Demo d=new Demo();
+        try{
+            d.div();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
