c80527ccb7883536ef2377295669b7834f2bb3f3
Anupama_Ravala <anupamaravala@gmail.com>
2025-12-18 18:57:34 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,13 @@
+public class Pattern1{
+public static void main(String[] args){
+        for(int i=1;i<=4;i++){
+            for(int j=i;j>=4;j--){
+                for(int k=1;k<=2*i+1;k++){
+                    System.out.print("*");
+                }
+            }
+          System.out.println(" "); 
+        }
+         
+    }
+}
